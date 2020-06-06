c1ae659723184e348c94828c94066eaf69cc36d2
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-06 03:34:16 -0400

    reverted back from location.state

--- src/pages/teleworking-technology-and-tools.tsx
+++ src/pages/teleworking-technology-and-tools.tsx
@@ -30,9 +30,12 @@ const TeleworkingTechnologyAndToolsCoursePage = ({ location }) => {
   return (
     <CourseLayout
       data={data}
-      courseName={location.state.courseName || 'loading'}
-      courseImage={location.state.courseImage || 'loading'}
-      courseDescription={location.state.courseDescription || 'loading'}
+      courseName="Teleworking Technology & Tools"
+      courseImage="https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+      courseDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolorem tempora ducimus deserunt porro aliquid nulla perferendis ab facere non?"
+      // courseName={location.state.courseName || 'loading'}
+      // courseImage={location.state.courseImage || 'loading'}
+      // courseDescription={location.state.courseDescription || 'loading'}
     />
   );
 };
